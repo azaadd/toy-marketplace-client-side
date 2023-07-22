69409d577a5a44a4162319c7fba4beffb8bf186c
azaadd <abulkalam.kta@gmail.com>
2023-07-22 12:28:35 +0600

    added view details page and add its content dynamically

--- src/pages/Home/shopcategory/ViewDetails.js
+++ src/pages/Home/shopcategory/ViewDetails.js
@@ -1,38 +1,49 @@
-import React, { } from 'react';
-// import { useLoaderData, useParams } from 'react-router-dom';
-// import SingleDetail from './SingleDetail';
+import React, { useEffect } from 'react';
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
+import { useLoaderData } from 'react-router-dom';
+
 
 const ViewDetails = () => {
 
-    // const details = useLoaderData();
+    const detailData = useLoaderData();
 
-    // // const { img, sellerName, email, toyName, subCategory, price, availableQuantity, rating, description
-    // //     } = _id;
+    const { img, sellerName, email, toyName, subCategory, price, availableQuantity, rating, description } = detailData;
 
-    
-    // const { id } = useParams();
-    // const [detail, setDetail] = useState([]);
-
-    // useEffect(() => {
-    //     const matchData = details.find(detail => detail._id === parseInt(id));
-    //     if (matchData) {
-    //         setDetail(matchData)
-    //     }
-    // }, []);
-
-    // console.log(detail);
-
+    console.log(detailData)
+    useEffect(() => {
+        document.title = `Shop Toy | ${toyName}`;
+    }, [])
 
     return (
         <div>
-            {/* {
-                details.map(detail => <SingleDetail
-                key={detail._id}
-                detail={detail}
-                >
+            <div className="hero pt-4 bg-base-200">
+                <div className="hero-content flex-col lg:flex-row gap-16">
+                    <img src={img} className="md:w-[625px] md:h-[370px] rounded-lg shadow-2xl" alt='' />
+                    <div>
+                        <h1 className="text-5xl font-bold">{toyName}</h1>
+                        <p className="py-4 font-semibold"><strong>Seller Name: </strong>{sellerName}</p>
+                        <p className="py-4 font-semibold"><strong>Seller e-mail: </strong>{email}</p>
+                        <p className="py-4 font-semibold"><strong>Price: </strong>$ {price}</p>
+                        
+                        <div className='flex items-center font-semibold'>
+                            <p className='py-4 font-semibold mr-6'><strong>Ratings: </strong> </p>
+                            <Rating
 
-                </SingleDetail>)
-            } */}
+                                placeholderRating={rating}
+
+                                emptySymbol={<FaRegStar className='fs-4'></FaRegStar>}
+                                placeholderSymbol={<FaStar className='text-warning fs-4'></FaStar>}
+                                fullSymbol={<FaStar className='text-warning fs-4'></FaStar>}
+                            ></Rating>
+                            <span className='fs-5 bg-primary-subtle px-3 rounded-5'>{rating}</span>
+                        </div>
+                        <p className="py-4 font-semibold"><strong>Available Quantity: </strong>{availableQuantity}</p>
+                        <p className="py-4"><strong>Description: </strong>{description}</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
