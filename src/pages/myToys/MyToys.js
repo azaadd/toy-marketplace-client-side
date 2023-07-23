7d2c6a4a26bf94a224ea1f477e4bc89d0eb2e191
azaadd <abulkalam.kta@gmail.com>
2023-07-23 18:11:32 +0600

    added My Toys page and elements

--- src/pages/myToys/MyToys.js
+++ src/pages/myToys/MyToys.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import MyToysBody from './MyToysBody';
 
 const MyToys = () => {
 
@@ -17,7 +18,37 @@ const MyToys = () => {
 
     return (
         <div className="overflow-x-auto mx-16 mt-8 mb-12">
-            <p>seller: {sellers.length}</p>
+            <div className='flex gap-6'>
+                <img className='w-28 h-28 rounded-sm' src={user.photoURL ? user.photoURL : ''} alt="" />
+                <div>
+                    <p className='text-2xl font-semibold text-gray-700'>{user?.displayName}</p>
+                    <p className='font-semibold text-gray-700'>Email: {user?.email}</p>
+                    <p className='text-xl font-semibold text-gray-700'>Total Items: {sellers?.length}</p>
+                </div>
+            </div>
+
+            <table className="table mt-3">
+                {/* head */}
+                <thead className='font-semibold bg-gray-200 text-black text-lg'>
+                    <tr>
+                        <th>Photo</th>
+                        <th>Toy Name</th>
+                        <th>Sub-Category</th>
+                        <th>Price</th>
+                        <th>Available Quantity</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+
+                {
+                    sellers.map(seller => <MyToysBody
+                        key={seller._id}
+                        seller={seller}
+                    ></MyToysBody>)
+                }
+
+
+            </table>
 
         </div>
     );
---
+++ src/pages/myToys/MyToysBody.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const MyToysBody = ({seller}) => {
+
+    const {
+        _id, name, email, toyname, photo, quantity, price, category
+        } = seller;
+
+    return (
+        <tbody className='h-36 text-[18px] font-semibold'>
+            {/* row 1 */}
+            <tr>
+
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-28 h-28">
+                                <img className='' src={photo} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </td> 
+                <td>{toyname}</td>
+                <td>{category}</td>
+                <td>$ {price}.00 Only</td>
+                <td>{quantity} PCS</td>
+                
+                <th>
+                    <Link to='' className="btn bg-green-600 btn-sm text-white hover:bg-gray-400"><FaEdit /> Edit</Link>
+                    <Link to='' className="btn bg-red-600 btn-sm text-white hover:bg-gray-400 ml-1"><FaTrash /> Delete</Link>
+                </th>
+            </tr>
+
+        </tbody>
+    );
+};
+
+export default MyToysBody;
